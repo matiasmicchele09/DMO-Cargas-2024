f2b6e126b1286a93625ededc960ab3dcc8c865c3
Matias Micchele <matias.micchele@hotmail.com>
2024-03-07 21:46:28 -0300

    primer avance sobre el home-page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'home', component: HomePageComponent
+  },
+  { //Cualquier otra ruta que no este definida en mi routing module quiero que redirija a home
+    path:'**', redirectTo: 'home'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
