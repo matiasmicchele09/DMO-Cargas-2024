01e83530dfe0eead03c0828c768eeb8c44c55ffd
Matias Micchele <matias.micchele@hotmail.com>
2024-03-09 16:44:16 -0300

    Agregado de sección 3 y footer

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent {
+  constructor(private elementRef: ElementRef) {} /* Para obtener una referencia a los elementos de las secciones */
+
+  @HostListener('window:scroll', ['$event']) /* Para detectar los eventos de desplazamiento de la ventana */
+  checkScroll() {
+    const sections = this.elementRef.nativeElement.querySelectorAll('.seccionDos');
+
+    sections.forEach((section: HTMLElement) => {
+      /* Iteramos sobre todas las secciones y comprobamos si la sección está en la mitad de la ventana verticalmente utilizando getBoundingClientRect() */
+      const rect = section.getBoundingClientRect();
+      const windowHeight = window.innerHeight;
+
+      /* Agregamos la clase */
+      if (rect.top < windowHeight / 1.5 && rect.bottom > windowHeight / 1.5) {
+        section.classList.add('animate__animated');
+        section.classList.add('animate__fadeInRightBig');
+      }
+    });
+  }
 
 }
